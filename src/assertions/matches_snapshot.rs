813357512286e63885fbe1cd540b62b3a09dfb11
Aaron Abramov <dabramov@fb.com>
2020-06-12 13:39:49 -0500

    buck support

--- src/assertions/matches_snapshot.rs
+++ src/assertions/matches_snapshot.rs
@@ -72,10 +72,27 @@ macro_rules! assert_matches_snapshot_r {
     }};
 }
 
-pub fn get_source_file_path(file: &str) -> PathBuf {
+fn get_project_root_path() -> PathBuf {
+    let buck_build_id_present = std::env::var("BUCK_BUILD_ID").is_ok();
+
+    // If there's a buck build id we'll grab the `pwd`, because we're probably running `buck test` from the root
+    if buck_build_id_present {
+        let pwd = std::env::var("PWD").expect(
+            "
+`BUCK_BUILD_ID` enviroment variable was present, which means this project is being built with
+buck and relies on `PWD` env variable to contain the project root, but `PWD` wasn't there",
+        );
+        return PathBuf::from(pwd);
+    }
+
+    // otherwise ask cargo for project root
     let project_root =
         std::env::var("CARGO_MANIFEST_DIR").expect("Can't get project root directory");
-    let mut p = PathBuf::from(project_root);
+    PathBuf::from(project_root)
+}
+
+pub fn get_source_file_path(file: &str) -> PathBuf {
+    let mut p = get_project_root_path();
     p.push(file);
     p
 }
