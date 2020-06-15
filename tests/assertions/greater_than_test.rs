9dbabca16c9427133ca2115058b25f5fd0d42835
TheWebDevel <sathish@skcript.com>
2020-06-15 22:31:54 +0530

    ran rustfmt

--- tests/assertions/greater_than_test.rs
+++ tests/assertions/greater_than_test.rs
@@ -3,21 +3,21 @@ use k9::{assert_greater_than, assert_matches_snapshot};
 
 #[test]
 fn test_assert_greater_than() -> Result<()> {
-  super::setup_test_env();
+    super::setup_test_env();
 
-  assert!(assert_greater_than!(2, 1).is_none());
+    assert!(assert_greater_than!(2, 1).is_none());
 
-  let failure_message = assert_greater_than!(1, 2).expect("must fail");
-  assert_matches_snapshot!(failure_message);
+    let failure_message = assert_greater_than!(1, 2).expect("must fail");
+    assert_matches_snapshot!(failure_message);
 
-  assert!(assert_greater_than!(9.8, 3.14, "Expected left to be greater than right").is_none());
-  Ok(())
+    assert!(assert_greater_than!(9.8, 3.14, "Expected left to be greater than right").is_none());
+    Ok(())
 }
 
 #[test]
 fn with_context() {
-  super::setup_test_env();
-  let err =
-    assert_greater_than!(1, 2, "Expected left to be greater than right").expect("must fail");
-  assert_matches_snapshot!(err);
+    super::setup_test_env();
+    let err =
+        assert_greater_than!(1, 2, "Expected left to be greater than right").expect("must fail");
+    assert_matches_snapshot!(err);
 }
