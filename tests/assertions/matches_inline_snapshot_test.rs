8fe7ceccd84e893f579292f10873c4dc09b5a4c0
Aaron Abramov <dabramov@fb.com>
2020-06-23 11:47:34 -0500

    rustfmt

--- tests/assertions/matches_inline_snapshot_test.rs
+++ tests/assertions/matches_inline_snapshot_test.rs
@@ -4,6 +4,6 @@ use crate::assert_matches_inline_snapshot;
 fn test_matches_inline_snapshot() {
     assert_matches_inline_snapshot!(
         "1234567890".repeat(5).chars().rev().collect::<String>(),
-         "09876543210987654321098765432109876543210987654321"
+        "09876543210987654321098765432109876543210987654321"
     );
 }
