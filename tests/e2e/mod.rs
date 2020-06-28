e53ce4a344415952aae9eef3a65b303ed6545194
TheWebDevel <sathish@skcript.com>
2020-06-28 10:50:42 +0530

    initial output parser

--- tests/e2e/mod.rs
+++ tests/e2e/mod.rs
@@ -1,7 +1,15 @@
 mod test_utils;
 
 use anyhow::Result;
+use regex::RegexBuilder;
+use std::collections::BTreeMap;
 use test_utils::TestProject;
+
+#[derive(Debug)]
+enum TestResult {
+    PASS,
+    FAIL,
+}
 
 #[test]
 fn inline_snapshots() -> Result<()> {
@@ -28,7 +36,20 @@ fn passing() {}
     )?;
 
     let output = p.run("cargo", &["test"])?;
-
-    println!("{}", String::from_utf8(output.stdout)?);
+    let regex = RegexBuilder::new("^test (?P<test>\\w+) \\.{3} (?P<result>FAILED|ok)$")
+        .multi_line(true)
+        .build()
+        .unwrap();
+    let output_str = &String::from_utf8(output.stdout)?;
+    let captures = regex.captures_iter(output_str);
+    let mut test_results: BTreeMap<String, TestResult> = BTreeMap::new();
+    for capture in captures {
+        if &capture["test"] == "ok" {
+            test_results.insert(capture["test"].to_string(), TestResult::PASS);
+        } else if &capture["test"] == "FAILED" {
+            test_results.insert(capture["test"].to_string(), TestResult::FAIL);
+        }
+    }
+    dbg!(test_results);
     Ok(())
 }
