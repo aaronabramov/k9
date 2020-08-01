0bf95ec0ec862483173206183b19e849038ffa42
Aaron Abramov <dabramov@fb.com>
2020-07-31 21:20:45 -0500

    concurrent string implementation for snapshots

--- src/lib.rs
+++ src/lib.rs
@@ -8,8 +8,11 @@ pub mod string_diff;
 
 mod constants;
 mod paths;
+mod snap;
 mod types;
 mod utils;
+
+pub use snap::Snap;
 
 // re-export things so macros have access to them
 pub mod __macros__ {
