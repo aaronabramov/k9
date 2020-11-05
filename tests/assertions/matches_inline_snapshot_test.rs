098e81dc8f9334d8f318b50c02f95311c204b526
Aaron Abramov <dmitrii@abramov.io>
2020-11-05 17:48:21 -0600

    use raw string literals in inline snapshots (#65)

--- tests/assertions/matches_inline_snapshot_test.rs
+++ tests/assertions/matches_inline_snapshot_test.rs
@@ -4,6 +4,16 @@ use crate::assert_matches_inline_snapshot;
 fn test_matches_inline_snapshot() {
     assert_matches_inline_snapshot!(
         "1234567890".repeat(5).chars().rev().collect::<String>(),
-        "09876543210987654321098765432109876543210987654321"
+        r##"09876543210987654321098765432109876543210987654321"##
+    );
+
+    assert_matches_inline_snapshot!(
+        r#"escaped string \": \"{\\"namespace\\":\\"www\\",\\"tim"#,
+        r##"escaped string \": \"{\\"namespace\\":\\"www\\",\\"tim"##
+    );
+
+    assert_matches_inline_snapshot!(
+        "escaped string  #r\"blahblah\"#",
+        r##"escaped string  #r"blahblah"#"##
     );
 }
