b86d3814e0108275647ba9e954f96934eacd9fa8
Aaron Abramov <dabramov@fb.com>
2020-11-08 12:25:07 -0600

    Use Deref trait in assert_equal! marco to compare vaules of different types

--- src/assertions/equal.rs
+++ src/assertions/equal.rs
@@ -2,8 +2,12 @@ use crate::string_diff::colored_diff;
 use colored::*;
 use std::fmt::Debug;
 
-pub fn assert_equal<T: Debug + PartialEq>(left: T, right: T) -> Option<String> {
-    if left != right {
+pub fn assert_equal<T1: Debug + PartialEq, T2: Debug + PartialEq>(
+    left: T1,
+    right: T2,
+    fail: bool,
+) -> Option<String> {
+    if fail {
         let diff_string = colored_diff(&format!("{:#?}", &left), &format!("{:#?}", &right))
             .unwrap_or_else(|| "no visual difference between values".to_string());
 
