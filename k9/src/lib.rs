f7f99f7280fb0d6eed2dc8a34fe2e0dfee8366d1
Aaron Abramov <aaron@abramov.io>
2020-12-06 22:27:38 -0600

    MultilineString + docs (#88)

--- k9/src/lib.rs
+++ k9/src/lib.rs
@@ -1,5 +1,5 @@
 /*!
-see https://github.com/aaronabramov/k9 for high level description of the library
+see <https://github.com/aaronabramov/k9> for high level description of the library
 */
 
 #![cfg_attr(feature = "custom_comparison_formatters", feature(specialization))]
@@ -8,11 +8,13 @@ pub mod assertions;
 pub mod config;
 pub mod string_diff;
 
+mod multiline_string;
 mod paths;
 mod snap;
 mod types;
 mod utils;
 
+pub use multiline_string::MultilineString;
 pub use snap::Snap;
 
 // re-export things so macros have access to them
