86dba245245772c3dcd0dcbdfb0a7a4db185ef58
Aaron Abramov <dabramov@fb.com>
2020-12-06 21:03:00 -0600

    Merge two tests into one

--- k9_tests/inline_snap_test.rs
+++ k9_tests/inline_snap_test.rs
@@ -1,6 +1,5 @@
 use crate::test_utils::TestProject;
 use anyhow::Result;
-use k9_released::*;
 
 #[test]
 fn inline_snapshots() -> Result<()> {
@@ -24,72 +23,6 @@ use k9::*;
 #[test]
 fn inline_snapshot() {
     assert_matches_inline_snapshot!(format!("{}", std::f64::consts::E));
-}
-
-#[test]
-fn passing() {}
-    "#,
-    )?;
-
-    let runner = p.run_tests().build().unwrap();
-    let test_run = runner.run()?;
-
-    assert!(!test_run.success);
-
-    dbg!(&test_run);
-    assert_matches_inline_snapshot!(
-        format!("{:?}", test_run.test_cases),
-        "{\"basic_tests::inline_snapshot\": TestCaseResult { status: Fail }, \"basic_tests::passing\": TestCaseResult { status: Pass }}"
-    );
-    let runner = p.run_tests().update_snapshots(true).build().unwrap();
-    let test_run = runner.run()?;
-    assert!(test_run.success);
-
-    // Inline snapshot must be updated in the source.
-    // NOTE: we're using assert_equal! so we don't test inline snapshot feature
-    // using inline snapshots macro. If it's broken, the test could be broken as well
-    // and will give false positive.
-    assert_eq!(
-        p.read_file("basic_tests.rs")?.as_str(),
-        "use k9::*;
-
-#[test]
-fn inline_snapshot() {
-    assert_matches_inline_snapshot!(format!(\"{}\", std::f64::consts::E), r##\"2.718281828459045\"##);
-}
-
-#[test]
-fn passing() {}
-"
-    );
-
-    Ok(())
-}
-
-// when using k9, users can import it with `use k9;` statement, instead of
-// `use k9::*`; then, calling `k9::assert_matches_inline_snapshot!(...)` with
-// the trailing k9.  let's make sure that using it that way works.
-#[test]
-fn inline_snapshots_qualified() -> Result<()> {
-    let p = TestProject::new();
-
-    p.write_file("Cargo.toml", crate::test_utils::TEST_CARGO_TOML)?;
-
-    p.write_file(
-        "lib.rs",
-        r#"
-#[cfg(test)]
-mod tests;
-"#,
-    )?;
-
-    p.write_file(
-        "tests.rs",
-        r#"
-use k9;
-
-#[test]
-fn inline_snapshot() {
     k9::assert_matches_inline_snapshot!(format!("{}", std::f64::consts::E));
 }
 
@@ -103,9 +36,9 @@ fn passing() {}
 
     assert!(!test_run.success);
 
-    assert_matches_inline_snapshot!(
+    k9_released::assert_matches_inline_snapshot!(
         format!("{:?}", test_run.test_cases),
-        "{\"tests::inline_snapshot\": TestCaseResult { status: Fail }, \"tests::passing\": TestCaseResult { status: Pass }}"
+        "{\"basic_tests::inline_snapshot\": TestCaseResult { status: Fail }, \"basic_tests::passing\": TestCaseResult { status: Pass }}"
     );
     let runner = p.run_tests().update_snapshots(true).build().unwrap();
     let test_run = runner.run()?;
@@ -115,12 +48,13 @@ fn passing() {}
     // NOTE: we're using assert_equal! so we don't test inline snapshot feature
     // using inline snapshots macro. If it's broken, the test could be broken as well
     // and will give false positive.
-    assert_eq!(
-        p.read_file("tests.rs")?.as_str(),
-        "use k9;
+    k9_released::assert_equal!(
+        p.read_file("basic_tests.rs")?.as_str(),
+        "use k9::*;
 
 #[test]
 fn inline_snapshot() {
+    assert_matches_inline_snapshot!(format!(\"{}\", std::f64::consts::E), r##\"2.718281828459045\"##);
     k9::assert_matches_inline_snapshot!(
         format!(\"{}\", std::f64::consts::E),
         r##\"2.718281828459045\"##
