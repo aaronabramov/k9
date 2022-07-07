ba717c4165e12de357690b7e9547037ff695a9e3
Dan Gohman <dev@sunfishcode.online>
2022-07-06 17:19:38 -0700

    Migrate from term_size to terminal_size.
    The [`term_size`] crate is no longer maintained, and behaves incorrectly
    [on PowerPC]. This patch migrates k9 to the [`terminal_size`] crate
    instead.
    
    [`term_size`]: https://crates.io/crates/term_size
    [`terminal_size`]: https://crates.io/crates/terminal_size
    [on PowerPC]: https://github.com/ogham/exa/issues/400

--- k9/src/utils.rs
+++ k9/src/utils.rs
@@ -6,8 +6,8 @@ pub fn terminal_separator_line() -> String {
     let width_override = crate::config::terminal_width_override();
     let width = if width_override != 0 {
         width_override
-    } else if let Some((width, _)) = term_size::dimensions() {
-        width
+    } else if let Some((width, _)) = terminal_size::terminal_size() {
+        width.0 as usize
     } else {
         100 // default width if we can't determine terminal width
     };
